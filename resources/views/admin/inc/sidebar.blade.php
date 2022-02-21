8625e2cc3b74e6d3d326e279ce71c32707bfdd11
Marshall Ovierdo <shrallvierdo@gmail.com>
2022-02-21 08:45:12 +0700

    admin - footer

---
+++ resources/views/admin/inc/sidebar.blade.php
@@ -0,0 +1,41 @@
+<div class="col-span-2 bg-slate-900 text-slate-300 flex flex-col gap-y-2 p-4">
+    <div class="flex items-center gap-x-2"><span class="fa fa-fw fa-list"></span>Konten Web</div>
+    <div class="flex flex-col pl-8">
+        <a href="#" class="hover:text-white">Iklan Utama</a>
+        <a href="#" class="hover:text-white">Halaman Web</a>
+        <a href="#" class="hover:text-white">Halaman Samping</a>
+        <a href="#" class="hover:text-white">Berita / Artikel</a>
+        <a href="#" class="hover:text-white">Buku Tamu</a>
+        <a href="#" class="hover:text-white">Layanan</a>
+        <a href="#" class="hover:text-white">Anggota</a>
+        <a href="#" class="hover:text-white">Galeri Gambar</a>
+        <a href="#" class="hover:text-white">Ongkos Kirim</a>
+    </div>
+    <div class="flex items-center gap-x-2"><span class="fa fa-fw fa-shopping-cart"></span>Toko</div>
+    <div class="flex flex-col pl-8">
+        <a href="#" class="hover:text-white">Member List</a>
+        <a href="#" class="hover:text-white">Delivery Cost</a>
+        <a href="#" class="hover:text-white">Product Category & Sub</a>
+        <a href="#" class="hover:text-white">Product Brand</a>
+        <a href="#" class="hover:text-white">Product</a>
+        <a href="#" class="hover:text-white">Product On Sale</a>
+        <a href="#" class="hover:text-white">Product Complimentary</a>
+        <a href="#" class="hover:text-white">Product Restock</a>
+        <a href="#" class="hover:text-white">Product Sold Out</a>
+        <a href="#" class="hover:text-white">Product Non-Active</a>
+        <a href="#" class="hover:text-white">Discount Coupon</a>
+        <a href="#" class="hover:text-white">Sales Report</a>
+        <a href="#" class="hover:text-white">Profit Report</a>
+    </div>
+    <div class="flex items-center gap-x-2"><span class="fa fa-fw fa-gear"></span>Pengaturan & Laporan</div>
+    <div class="flex flex-col pl-8">
+        <a href="#" class="hover:text-white">Konfigurasi</a>
+        <a href="#" class="hover:text-white">Layout & Design</a>
+        <a href="#" class="hover:text-white">Administrator</a>
+        <a href="#" class="hover:text-white">Hit Counter</a>
+        <a href="#" class="hover:text-white">Top Visitor</a>
+        <a href="#" class="hover:text-white">Send Mail</a>
+        <a href="#" class="hover:text-white">Webmail</a>
+        <a href="#" class="hover:text-white">Tutorial</a>
+    </div>
+</div>
