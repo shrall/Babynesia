7316eab78a4f51a3d7083e68b7638f578a176965
akristianto03 <63012597+akristianto03@users.noreply.github.com>
2022-04-15 20:25:58 +0700

    Update GuestbookController.php

--- app/Http/Controllers/GuestbookController.php
+++ app/Http/Controllers/GuestbookController.php
@@ -19,20 +19,9 @@ class GuestbookController extends Controller
     public function index()
     {
         $page = 'guestbook';
-        $allkategoris = Kategori::orderBy('no_kategori', 'desc')->get();
-        $subkategoris = KategoriChild::all();
-
         $guestbooks = Guestbook::all();
 
-        //get color webconfig
-        $bg_color = Webconfig::where('name', 'bg_color')->get()->last();
-        $text_color = Webconfig::where('name', 'text_color')->get()->last();
-        $button_color = Webconfig::where('name', 'button_color')->get()->last();
-        $color = [$bg_color->content, $text_color->content, $button_color->content];
-        //background image
-        $bg_img = Webconfig::where('name', 'bg_img')->get()->last();
-
-        return view('user.guestbook', compact('page', 'allkategoris', 'subkategoris', 'guestbooks', 'color', 'bg_img'));
+        return view('user.guestbook', compact('page', 'guestbooks'));
     }
 
     /**
