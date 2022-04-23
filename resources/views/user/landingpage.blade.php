b06cb8fa353920f13da0863b819a8e2efa87a2a6
Marshall Ovierdo <shrallvierdo@gmail.com>
2022-04-23 11:55:33 +0700

    fix image src

--- resources/views/user/landingpage.blade.php
+++ resources/views/user/landingpage.blade.php
@@ -81,7 +81,7 @@
         <div class="grid grid-cols-2 gap-2 sm:grid-cols-4 sm:gap-3 xl:mx-auto">
             @foreach ($produks as $produk)
             <a href="{{ route('produk.show', $produk) }}" class="rounded-lg shadow-sm bg-white">
-                <img src="http://www.tokobayifiv.com/images/produk/{{ $produk->image }}"
+                <img src="{{ asset('uploads/') . '/' . $produk->image }}"
                     class="aspect-square w-full bg-gray-400 rounded-t-lg object-cover" alt="">
                 <div class="p-4 pb-6">
                     <h6 class="font-encode-sans font-bold sm:text-base text-sm text-clip">
@@ -132,7 +132,7 @@
         <div class="grid grid-cols-2 gap-2 sm:grid-cols-4 sm:gap-3 mb-7 xl:mx-auto">
             @foreach ($featured as $produk)
             <a href="{{ route('produk.show', $produk) }}" class="rounded-lg shadow-sm bg-white">
-                <img src="http://www.tokobayifiv.com/images/produk/{{ $produk->image }}"
+                <img src="{{ asset('uploads/') . '/' . $produk->image }}"
                     class="aspect-square w-full bg-gray-400 rounded-t-lg object-cover" alt="">
                 <div class="p-4 pb-6">
                     <h6 class="font-encode-sans font-bold sm:text-base text-sm text-clip">
