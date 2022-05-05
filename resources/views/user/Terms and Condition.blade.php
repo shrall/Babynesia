9bd3c864c3c46bcf05ac0b95d01927da20200ae0
Marshall Ovierdo <shrallvierdo@gmail.com>
2022-05-05 21:01:42 +0700

    tnc

--- resources/views/user/Terms and Condition.blade.php
+++ resources/views/user/Terms and Condition.blade.php
@@ -3,5 +3,21 @@
 
 @include('inc.navbar1')
 
+<div class="container mx-auto xl:px-32 px-3 xl:pt-6 xl:pb-10 pt-3 pb-5">
+    <div class="w-full bg-white rounded-md shadow-sm pt-3 pb-3 px-3">
+        <h1 class="font-concert-one text-3xl text-{{ $color[1] }}-500 xl:text-4xl">
+            {{ $sites->code }}
+        </h1>
+    </div>
+
+    <div class="w-full mt-3 bg-white rounded-md shadow-sm pt-3 pb-7 px-3">
+        <div class="">
+            <p class="font-encode-sans text-gray-400 text-sm sm:text-base">
+                {!! $sites->isi !!}
+            </p>
+        </div>
+    </div>
+</div>
+
 @include('inc.footer1')
 @endsection
