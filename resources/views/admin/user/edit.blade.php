38df3fc4e8daa3d2ba180fd860eaa7fa2ca0dba9
Marshall Ovierdo <shrallvierdo@gmail.com>
2022-05-18 09:52:00 +0700

    Update edit.blade.php

--- resources/views/admin/user/edit.blade.php
+++ resources/views/admin/user/edit.blade.php
@@ -33,12 +33,51 @@
             </div>
             <div class="col-span-3">Status</div>
             <div class="col-span-9 flex items-center gap-x-2">
-                <select name="status" id="status" class="admin-input" required>
-                    @foreach ($statuses as $status)
-                        <option value="{{ $status->id }}" {{ $user->user_status_id == $status->id ? 'selected' : '' }}>
-                            {{ $status->user_status }}</option>
-                    @endforeach
-                </select>
+                @if (Auth::user()->user_status_id != 6 && Auth::user()->user_status_id != 7)
+                    @if ($user->user_status_id == 1 || $user->user_status_id == 2)
+                        <select name="status" id="status" class="admin-input" required>
+                            @foreach ($statuses as $status)
+                                @if ($status->id == 1 || $status->id == 2)
+                                    <option value="{{ $status->id }}"
+                                        {{ $user->user_status_id == $status->id ? 'selected' : '' }}>
+                                        {{ $status->user_status }}</option>
+                                @endif
+                            @endforeach
+                        </select>
+                    @else
+                        <select name="status" id="status" class="admin-input" required>
+                            @foreach ($statuses as $status)
+                                @if (Auth::user()->user_status_id == 4)
+                                    @if ($status->id == 4 || $status->id == 6)
+                                        <option value="{{ $status->id }}"
+                                            {{ $user->user_status_id == $status->id ? 'selected' : '' }}>
+                                            {{ $status->user_status }}</option>
+                                    @endif
+                                @elseif (Auth::user()->user_status_id == 5)
+                                    @if ($status->id == 5 || $status->id == 7)
+                                        <option value="{{ $status->id }}"
+                                            {{ $user->user_status_id == $status->id ? 'selected' : '' }}>
+                                            {{ $status->user_status }}</option>
+                                    @endif
+                                @else
+                                    @if ($status->id != 1 && $status->id != 2)
+                                        <option value="{{ $status->id }}"
+                                            {{ $user->user_status_id == $status->id ? 'selected' : '' }}>
+                                            {{ $status->user_status }}</option>
+                                    @endif
+                                @endif
+                            @endforeach
+                        </select>
+                    @endif
+                @else
+                    <select name="status" id="status" class="admin-input" required disabled>
+                        @foreach ($statuses as $status)
+                            <option value="{{ $status->id }}"
+                                {{ $user->user_status_id == $status->id ? 'selected' : '' }}>
+                                {{ $status->user_status }}</option>
+                        @endforeach
+                    </select>
+                @endif
             </div>
             <div class="col-span-3">Nama</div>
             <div class="col-span-9 flex items-center gap-x-2">
@@ -55,8 +94,8 @@
             </div>
             <div class="col-span-3">Kode Pos</div>
             <div class="col-span-9 flex items-center gap-x-2">
-                <input type="text" name="postal_code" id="postal_code" class="admin-input" value="{{ $user->kodepos }}"
-                    required>
+                <input type="text" name="postal_code" id="postal_code" class="admin-input"
+                    value="{{ $user->kodepos }}" required>
             </div>
             <div class="col-span-3">Negara</div>
             <div class="col-span-9 flex items-center gap-x-2">
