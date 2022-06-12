03b527839876543854cf5749240d066033621f7c
Marshall Ovierdo <shrallvierdo@gmail.com>
2022-06-12 19:33:34 +0700

    chore: paymentmethod seeder set to default

--- database/seeders/PaymentMethodSeeder.php
+++ database/seeders/PaymentMethodSeeder.php
@@ -14,17 +14,17 @@ class PaymentMethodSeeder extends Seeder
      */
     public function run()
     {
-        // $status = new PaymentMethod();
-        // $status->name = 'bank_1';
-        // $status->info = 'BCA 0101';
-        // $status->description = 'BCA 0101-66-2828, an Rita Koeswatie';
-        // $status->save();
+        $status = new PaymentMethod();
+        $status->name = 'bank_1';
+        $status->info = 'BCA 0101';
+        $status->description = 'BCA 0101-66-2828, an Rita Koeswatie';
+        $status->save();
 
-        // $status = new PaymentMethod();
-        // $status->name = 'bank_2';
-        // $status->info = 'Mandiri 144';
-        // $status->description = 'Mandiri 144-0011-852-032, an Rita Koeswatie';
-        // $status->save();
+        $status = new PaymentMethod();
+        $status->name = 'bank_2';
+        $status->info = 'Mandiri 144';
+        $status->description = 'Mandiri 144-0011-852-032, an Rita Koeswatie';
+        $status->save();
 
         $status = new PaymentMethod();
         $status->name = 'bank_3';
