58bc773ce125bb65a85c16d819d46a1db3a3ac35
Marshall Ovierdo <shrallvierdo@gmail.com>
2022-06-12 19:32:19 +0700

    feat: new paymentmethod seeder

--- database/seeders/PaymentMethodSeeder.php
+++ database/seeders/PaymentMethodSeeder.php
@@ -14,16 +14,34 @@ class PaymentMethodSeeder extends Seeder
      */
     public function run()
     {
+        // $status = new PaymentMethod();
+        // $status->name = 'bank_1';
+        // $status->info = 'BCA 0101';
+        // $status->description = 'BCA 0101-66-2828, an Rita Koeswatie';
+        // $status->save();
+
+        // $status = new PaymentMethod();
+        // $status->name = 'bank_2';
+        // $status->info = 'Mandiri 144';
+        // $status->description = 'Mandiri 144-0011-852-032, an Rita Koeswatie';
+        // $status->save();
+
         $status = new PaymentMethod();
-        $status->name = 'bank_1';
-        $status->info = 'BCA 0101';
-        $status->description = 'BCA 0101-66-2828, an Rita Koeswatie';
+        $status->name = 'bank_3';
+        $status->info = 'ShopeePay';
+        $status->description = 'ShopeePay 082232550468 , an Rita Koeswatie';
         $status->save();
 
         $status = new PaymentMethod();
-        $status->name = 'bank_2';
-        $status->info = 'Mandiri 144';
-        $status->description = 'Mandiri 144-0011-852-032, an Rita Koeswatie';
+        $status->name = 'bank_4';
+        $status->info = 'OVO';
+        $status->description = 'OVO 082232550468 , an Rita Koeswatie';
+        $status->save();
+
+        $status = new PaymentMethod();
+        $status->name = 'bank_5';
+        $status->info = 'GoPay';
+        $status->description = 'GoPay 082232550468 , an Rita Koeswatie';
         $status->save();
     }
 }
