1882835271f90d8551ddcd91ca29dd2979d4bb02
Marshall Ovierdo <shrallvierdo@gmail.com>
2022-07-22 21:44:17 +0700

    feat(admin): add voucher_id column to faktur table

--- app/Models/Faktur.php
+++ app/Models/Faktur.php
@@ -12,7 +12,7 @@ class Faktur extends Model
     protected $table = 'faktur';
     protected $fillable = [
         'no_faktur', 'kode_user', 'status', 'tanggal', 'cara_bayar', 'total_pembayaran', 'valuta_id', 'total_profit', 'deliverycost',
-        'deliveryDate', 'deliveryExpedition', 'deliveryResi', 'tanggal2', 'discount', 'note', 'admin_note', 'total_weight', 'sender_name', 'sender_phone', 'sender_address'
+        'deliveryDate', 'deliveryExpedition', 'deliveryResi', 'tanggal2', 'discount', 'note', 'admin_note', 'total_weight', 'sender_name', 'sender_phone', 'sender_address', 'voucher_id'
     ];
     protected $primaryKey = 'no_faktur';
     // public $incrementing = false;
@@ -35,5 +35,9 @@ class Faktur extends Model
     public function payment()
     {
         return $this->belongsTo(PaymentMethod::class, 'cara_bayar', 'name');
+    }
+    public function voucher()
+    {
+        return $this->belongsTo(Voucher::class, 'voucher_id', 'id');
     }
 }
