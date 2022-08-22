d6a1232e70d36d328e5112d6763eb3060eb38c85
Marshall Ovierdo <shrallvierdo@gmail.com>
2022-08-22 10:40:51 +0700

    feat(admin): button to go back to main web

--- resources/views/admin/inc/sidebar.blade.php
+++ resources/views/admin/inc/sidebar.blade.php
@@ -1,4 +1,8 @@
 <div class="col-span-2 bg-slate-900 text-slate-300 flex flex-col gap-y-2 p-4">
+    <div class="flex items-center gap-x-2"><span class="fa fa-fw fa-arrow-left"></span>
+        <a target="_blank" href="{{ route('landingpage') }}" class="hover:text-white">Kembali ke Website</a>
+    </div>
+    <hr>
     <div class="flex items-center gap-x-2"><span class="fa fa-fw fa-list"></span>Konten Web</div>
     <div class="flex flex-col pl-8">
         <a href="{{ route('adminpage.hotdeals.index') }}" class="hover:text-white">Iklan Utama</a>
@@ -21,7 +25,7 @@
         <a href="{{ route('adminpage.produk.index.soldout') }}" class="hover:text-white">Produk Sold Out</a>
         {{-- <a href="{{ route('adminpage.produk.index.disabled') }}" class="hover:text-white">Produk Non-Aktif</a> --}}
         {{-- <a href="#" class="hover:text-white">Produk Pelengkap</a> --}}
-        <a href="{{route('adminpage.voucher.index')}}" class="hover:text-white">Voucher Diskon</a>
+        <a href="{{ route('adminpage.voucher.index') }}" class="hover:text-white">Voucher Diskon</a>
         <a href="{{ route('adminpage.faktur.index') }}" class="hover:text-white">Laporan Penjualan</a>
         @if (Auth::user()->user_status_id != 6 && Auth::user()->user_status_id != 7)
             <a href="{{ route('adminpage.profit') }}" class="hover:text-white">Laporan Keuntungan</a>
