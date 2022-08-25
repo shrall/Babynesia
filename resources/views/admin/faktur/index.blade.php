0504082c225e62a013fbe18ec2449a50be825f9f
Marshall Ovierdo <shrallvierdo@gmail.com>
2022-08-25 08:43:44 +0700

    fix: carbon timezone

--- resources/views/admin/faktur/index.blade.php
+++ resources/views/admin/faktur/index.blade.php
@@ -71,8 +71,9 @@
                                 <td style="color: {{ $faktur->fakturstatus->color }};">
                                     {{ $faktur->fakturstatus->status }}</td>
                                 <td>{{ $faktur->tanggal2 }}</td>
-                                <td>{{ $faktur->user->name ?? ""}}</td>
-                                <td>{{ $faktur->receiver->receiver_name . ', ' . $faktur->receiver->city }}</td>
+                                <td>{{ $faktur->user->name ?? '' }}</td>
+                                <td>{{ ($faktur->receiver->receiver_name ?? '') . ', ' . ($faktur->receiver->city ?? '') }}
+                                </td>
                                 <td>{{ AppHelper::rp($faktur->total_profit ?? 0) }}</td>
                                 <td>{{ AppHelper::rp($faktur->discount ?? 0) }}</td>
                                 <td>{{ AppHelper::rp($faktur->deliverycost ?? 0) }}</td>
